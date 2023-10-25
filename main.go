40615b6c9de148180a4462dd0f5ba8a8e5b50722
hooss-only <hoossonly@gmail.com>
2023-10-25 16:17:30 +0900

    Added todo add and save

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"container/list"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -24,10 +25,13 @@ var commands = [...]command{
 	{name: "add", description: "add new todo", usage: "todo add <to-do>"},
 }
 
-var todos = list.New()
+var todos []todo
+var latestId = 0
+var homeFolder string
+var configFolder = homeFolder + ".config/todos-only/"
 
 func help(args []string) {
-	if len(args) == 2 {
+	if len(args) <= 2 {
 		fmt.Printf("Usage:\n\ttodo <command> [args..]\n\n")
 
 		fmt.Println("Commands:")
@@ -59,7 +63,52 @@ func help(args []string) {
 	return
 }
 
+func getHome() {
+	dirname, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println(err)
+	}
+	homeFolder = dirname
+}
+
+func checkFolder() bool {
+	if _, err := os.Stat(homeFolder + ".config"); os.IsNotExist(err) {
+		if err := os.Mkdir(homeFolder+".config", os.ModePerm); err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+		return false
+	}
+	if _, err := os.Stat(homeFolder + ".config/todos-only"); os.IsNotExist(err) {
+		if err := os.Mkdir(homeFolder+".config/todos-only", os.ModePerm); err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+		return false
+	}
+	return true
+}
+
+func save() {
+	dataString := strconv.Itoa(latestId) + "\n"
+
+	for _, todo := range todos {
+		dataString += strconv.Itoa(todo.id) + "," + todo.todo + "," + strconv.FormatBool(todo.check) + "\n"
+	}
+
+	data := []byte(dataString)
+	err := os.WriteFile(configFolder+"save.txt", data, 0644)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
 func main() {
+	if !checkFolder() {
+		checkFolder()
+	}
+
 	args := os.Args
 
 	if len(args) == 1 {
@@ -69,9 +118,11 @@ func main() {
 	} else {
 		existsCommand := false
 		cmd := strings.ToLower(args[1])
+		var commandInfinitive command
 		for _, c := range commands {
 			if c.name == cmd {
 				existsCommand = true
+				commandInfinitive = c
 			}
 		}
 
@@ -84,7 +135,22 @@ func main() {
 		case "help":
 			help(args)
 		case "add":
+			if len(args) < 3 {
+				fmt.Println(commandInfinitive.usage)
+				return
+			}
+			latestId++
+			var todoText string
+			for i, txt := range args[2:] {
+				todoText += txt
+				if !(i+1 == len(args[2:])) {
+					todoText += " "
+				}
+			}
 
+			todos = append(todos, todo{id: latestId, todo: todoText, check: false})
+
+			fmt.Println(todos)
 		}
 	}
 }
